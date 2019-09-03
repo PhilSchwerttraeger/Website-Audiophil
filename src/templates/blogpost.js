b4213205c6e7c4b69626489df443efb0f3421414
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-03 13:04:47 +0200

    Add dynamic blog post site generation from template

---
+++ src/templates/blogpost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../layout/layout'
+
+export default function blogpost() {
+  return (
+    <div>
+      <Layout>
+        This is Blog.
+      </Layout>
+    </div>
+  )
+}
