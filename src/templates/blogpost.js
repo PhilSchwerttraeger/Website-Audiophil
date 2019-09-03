821a158ee59e8d41f942d89193208143b5c42d2a
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-03 16:51:22 +0200

    Fetching post data to generated pages with graphql

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import Layout from '../layout/layout'
+import { graphql } from 'gatsby'
 
-export default function blogpost() {
+export const query = graphql`
+  query(
+    $slug: Date!
+  ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const blogpost = ({ data }) => {
   return (
     <div>
       <Layout>
-        This is Blog.
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <h2>{data.markdownRemark.frontmatter.date}</h2>
+        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
       </Layout>
     </div>
   )
 }
+
+export default blogpost
