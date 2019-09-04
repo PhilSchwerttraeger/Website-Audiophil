6de951e673d062973b6ee69e2906783658dcabb2
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-04 17:34:08 +0200

    Add Footer construct

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,9 +1,40 @@
 import React from "react"
 import footerStyles from "./footer.module.scss"
+import Grid from '@material-ui/core/Grid'
+import { Link } from 'gatsby'
+import Logo from './logo.js'
 
 const Footer = () =>
   <footer className={footerStyles.footer}>
-    © {new Date().getFullYear()}
+    <Grid container className={footerStyles.container}>
+      <Grid item>
+        <Logo />
+      </Grid>
+      <Grid item>
+        <ul>
+          <li>hi</li>
+          <li>das</li>
+          <li>dfg</li>
+        </ul>
+      </Grid>
+
+      <Grid item>
+        <ul>
+          <li>hi</li>
+          <li>das</li>
+          <li>dfg</li>
+        </ul>
+      </Grid>
+
+      <Grid item>
+
+      </Grid>
+
+      <Grid item>
+
+        © {new Date().getFullYear()}
+      </Grid>
+    </Grid>
   </footer>
 
 export default Footer
