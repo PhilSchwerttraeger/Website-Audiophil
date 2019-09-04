4e947b2f26c62f255128e9a7100a3f0cf9aead2e
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-04 15:22:51 +0200

    Fix sticky footer

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,18 @@ import React from "react"
 
 import Layout from "../layout/layout"
 import SEO from "../components/seo"
+import blogStyles from "./blog.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>Ups. Nix gefunden.</h1>
-    <p>Die Seite gibt es leider nicht. Da ist wohl was schief gelaufen.</p>
-  </Layout>
+    <main className={blogStyles.main}>
+      <SEO title="404: Not found" />
+      <div style={{ margin: "60px 0px 60px", textAlign: "center" }}>
+        <h1>Ups. Nix gefunden.</h1>
+        <p>Die Seite gibt es leider nicht. Da ist wohl was schief gelaufen.</p>
+      </div>
+    </main>
+  </Layout >
 )
 
 export default NotFoundPage
