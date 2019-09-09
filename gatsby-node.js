257428e1774f3e587dc10e0558ce8f10aaa0d0fc
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-09 13:12:32 +0200

    Add category pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
+  // BLOG POST PAGES
   // 1. Get path to template
   const blogTemplate = path.resolve("./src/templates/blogpost.js")
 
@@ -33,6 +34,40 @@ module.exports.createPages = async ({ graphql, actions }) => {
     createPage({
       component: blogTemplate,
       path: `/blog/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug
+      }
+    })
+  })
+
+  // CATEGORY PAGES
+  // 1. Get path to template
+  const categoryTemplate = path.resolve("./src/templates/categorypage.js")
+
+  // 2. Get data
+  const resCategories = await graphql(`
+    query{
+      allWordpressCategory {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  /*
+  resCategories.data.allWordpressCategory.edges.map(edge => {
+    console.log(edge.node.slug);
+  })
+  */
+
+  // 3. Create new pages
+  resCategories.data.allWordpressCategory.edges.forEach((edge) => {
+    createPage({
+      component: categoryTemplate,
+      path: `/category/${edge.node.slug}`,
       context: {
         slug: edge.node.slug
       }
