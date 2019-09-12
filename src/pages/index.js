247ead666c2115895c684cc35f7ab54dc095ce77
Philipp Schwerttraeger <ps@fkyp.de>
2019-09-12 10:39:07 +0200

    Add Google Analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import Grid from "@material-ui/core/Grid"
 
 import Layout from "../layout/layout"
-import SEO from "../components/seo"
-
 import layoutStyles from "../layout/layout.module.scss"
-
+import SEO from "../components/seo"
 import patch from "../images/patch.png"
-import Grid from "@material-ui/core/Grid"
+
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-142374149-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const hardNumbers = [
   {
@@ -93,199 +98,202 @@ const hardNumbers2 = [
   },
 ]
 
-const IndexPage = () => (
-  <Layout type="hero">
-    <SEO title="audiophil" />
+const IndexPage = () => {
+  initializeReactGA();
+  return (
+    <Layout type="hero">
+      <SEO title="audiophil" />
 
-    {/* SECTION A: WELCOME */}
-    <section id="section-a" className={layoutStyles.section}>
-      <div className={layoutStyles.contentWrap}>
-        <h2 id="section-a">Hallo Internet!</h2>
-        <div className={layoutStyles.contentText}>
-          <p>
-            Seitdem ich denken kann begeistere ich mich für Musik und Technik.
-            Seit 2013 arbeite ich mit Künstlern und Bands zusammen an ihren
-            Alben, vom Songwriting über Aufnahme bis hin zum Mastering und
-            Remix.
+      {/* SECTION A: WELCOME */}
+      <section id="section-a" className={layoutStyles.section}>
+        <div className={layoutStyles.contentWrap}>
+          <h2 id="section-a">Hallo Internet!</h2>
+          <div className={layoutStyles.contentText}>
+            <p>
+              Seitdem ich denken kann begeistere ich mich für Musik und Technik.
+              Seit 2013 arbeite ich mit Künstlern und Bands zusammen an ihren
+              Alben, vom Songwriting über Aufnahme bis hin zum Mastering und
+              Remix.
           </p>
-          <p>
-            Lange Zeit in improvisierten Räumlichkeiten in einem alten
-            Musterhaus, träumte ich davon ein eigenes Tonstudio zu bauen. Vor
-            einigen Jahren habe ich einen guten Freund kennengelernt, der
-            zusammen mit mir dieses Ziel erreichen möchte. Diese Seite enthält
+            <p>
+              Lange Zeit in improvisierten Räumlichkeiten in einem alten
+              Musterhaus, träumte ich davon ein eigenes Tonstudio zu bauen. Vor
+              einigen Jahren habe ich einen guten Freund kennengelernt, der
+              zusammen mit mir dieses Ziel erreichen möchte. Diese Seite enthält
             einen <Link to="/blog">Blog</Link>, in welchem ich den aktuellen
-            Stand unserer Planung und unseres Baus festhalte, aber auch
-            detailliert auf alle technischen und bauakustischen Hintergründe
-            eingehe.
+                  Stand unserer Planung und unseres Baus festhalte, aber auch
+                  detailliert auf alle technischen und bauakustischen Hintergründe
+                  eingehe.
           </p>
-          <p>
-            Da ich nahezu den ganzen Bau von der Planung bis zum fertigen Ausbau
-            selbst mache, möchte ich auf diesem Blog Wissen und Erfahrungen
-            sammeln, bündeln und der Welt wieder zurückgeben. Ich würde mich
-            freuen, wenn ich hiermit interessierte Menschen oder zukünftige
-            Studiobauer erreichen kann und ihnen bei der einen oder anderen
-            Sache mehr Durchblick verschaffen kann.
+            <p>
+              Da ich nahezu den ganzen Bau von der Planung bis zum fertigen Ausbau
+              selbst mache, möchte ich auf diesem Blog Wissen und Erfahrungen
+              sammeln, bündeln und der Welt wieder zurückgeben. Ich würde mich
+              freuen, wenn ich hiermit interessierte Menschen oder zukünftige
+              Studiobauer erreichen kann und ihnen bei der einen oder anderen
+              Sache mehr Durchblick verschaffen kann.
           </p>
-          <p>
-            Ich freue mich jederzeit über Kommentare und Kritik. Falls du
-            Interesse hast bei uns aufzunehmen, fühl dich frei uns einfach
-            anzuschreiben.
+            <p>
+              Ich freue mich jederzeit über Kommentare und Kritik. Falls du
+              Interesse hast bei uns aufzunehmen, fühl dich frei uns einfach
+              anzuschreiben.
           </p>
+          </div>
+          <div className={layoutStyles.centered}>
+            <Link to="/blog">
+              <button type="button" className={layoutStyles.button}>
+                <span>Zum Blog</span>
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className={layoutStyles.centered}>
-          <Link to="/blog">
-            <button type="button" className={layoutStyles.button}>
-              <span>Zum Blog</span>
-            </button>
-          </Link>
-        </div>
-      </div>
-    </section>
+      </section>
 
-    {/* SECTION B: FEATURES */}
-    <section id="section-b" className={layoutStyles.section}>
-      <div
-        className={layoutStyles.contentWrap}
-        style={{
-          backgroundImage: `url(${patch})`,
-          backgroundSize: "auto 100%",
-          backgroundRepeat: "no-repeat",
-          borderRadius: "25px",
-        }}
-      >
-        <h2 className={layoutStyles.featuresTitle} id="section-b">
-          Harte Zahlen
-        </h2>
-        <Grid
-          container
-          justify="center"
-          className={layoutStyles.featureContainer}
+      {/* SECTION B: FEATURES */}
+      <section id="section-b" className={layoutStyles.section}>
+        <div
+          className={layoutStyles.contentWrap}
+          style={{
+            backgroundImage: `url(${patch})`,
+            backgroundSize: "auto 100%",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "25px",
+          }}
         >
-          {hardNumbers.map(item => {
-            return (
-              <Grid
-                key={item.title}
-                item
-                xs={6}
-                md={3}
-                className={layoutStyles.feature}
-              >
-                <div className={layoutStyles.card}>
+          <h2 className={layoutStyles.featuresTitle} id="section-b">
+            Harte Zahlen
+        </h2>
+          <Grid
+            container
+            justify="center"
+            className={layoutStyles.featureContainer}
+          >
+            {hardNumbers.map(item => {
+              return (
+                <Grid
+                  key={item.title}
+                  item
+                  xs={6}
+                  md={3}
+                  className={layoutStyles.feature}
+                >
+                  <div className={layoutStyles.card}>
+                    <h3>{item.title}</h3>
+                    <h4>{item.subtitle}</h4>
+                    <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
+                  </div>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </div>
+      </section>
+
+      {/* SECTION C: DREAM */}
+      <section id="section-c" className={layoutStyles.section}>
+        <div className={layoutStyles.contentWrap}>
+          <h2 id="section-c">Was lange währt, wird endlich gut.</h2>
+          <div className={layoutStyles.contentText}>
+            <p>
+              Kaum zu glauben, aber bereits Anfang 2014 begann dieses Projekt.
+              Seitdem hat sich vieles verändert. Aus einem anfangs kleinen Anbau
+              ist mittlerweile ein autarkes Gebäude geworden. Auch die technischen
+              Anforderungen wuchsen mit der Zeit, sodass der Bau bauakustisch nun
+              sogar aller höchste Ansprüche erfüllt.
+          </p>
+            <p>
+              Eigentlich können wir froh sein, dass es die initiale Idee bzw.
+              Planung nicht geworden ist. Mit der Zeit habe ich nämlich enorm viel
+              gelernt. Nur durch das mehrfache Planen und immer wieder
+              Durchdenken, Rekapitulieren und Revalidieren verschiedener Aspekte
+              konnte das jetzige Studio entstehen.
+          </p>
+          </div>
+
+          <div className={layoutStyles.disciplines}>
+            {disciplines.map(item => {
+              return (
+                <div
+                  key={item.title}
+                  className={layoutStyles.discipline}
+                >
                   <h3>{item.title}</h3>
-                  <h4>{item.subtitle}</h4>
                   <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
                 </div>
-              </Grid>
-            )
-          })}
-        </Grid>
-      </div>
-    </section>
-
-    {/* SECTION C: DREAM */}
-    <section id="section-c" className={layoutStyles.section}>
-      <div className={layoutStyles.contentWrap}>
-        <h2 id="section-c">Was lange währt, wird endlich gut.</h2>
-        <div className={layoutStyles.contentText}>
-          <p>
-            Kaum zu glauben, aber bereits Anfang 2014 begann dieses Projekt.
-            Seitdem hat sich vieles verändert. Aus einem anfangs kleinen Anbau
-            ist mittlerweile ein autarkes Gebäude geworden. Auch die technischen
-            Anforderungen wuchsen mit der Zeit, sodass der Bau bauakustisch nun
-            sogar aller höchste Ansprüche erfüllt.
-          </p>
-          <p>
-            Eigentlich können wir froh sein, dass es die initiale Idee bzw.
-            Planung nicht geworden ist. Mit der Zeit habe ich nämlich enorm viel
-            gelernt. Nur durch das mehrfache Planen und immer wieder
-            Durchdenken, Rekapitulieren und Revalidieren verschiedener Aspekte
-            konnte das jetzige Studio entstehen.
-          </p>
+              )
+            })}
+          </div>
         </div>
+      </section>
 
-        <div className={layoutStyles.disciplines}>
-          {disciplines.map(item => {
-            return (
-              <div
-                key={item.title}
-                className={layoutStyles.discipline}
+      {/* SECTION D: Hard Numbers 2 */}
+      <section id="section-d" className={layoutStyles.section}>
+        <div
+          className={layoutStyles.contentWrap}
+          style={{
+            backgroundImage: `url(${patch})`,
+            backgroundSize: "100% 100%",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "25px",
+            paddingTop: "20px",
+          }}
+        >
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <Grid
+                container
+                justify="center"
+                className={layoutStyles.factsContainer}
               >
-                <h3>{item.title}</h3>
-                <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    </section>
+                {hardNumbers2.map(item => {
+                  return (
+                    <Grid
+                      key={item.value}
+                      item xs={6}
+                      className={layoutStyles.fact}
+                    >
+                      <div className={layoutStyles.card2}>
+                        <h3>{item.value}</h3>
+                        <h4>{item.unit}</h4>
+                      </div>
+                    </Grid>
+                  )
+                })}
+              </Grid>
+            </Grid>
 
-    {/* SECTION D: Hard Numbers 2 */}
-    <section id="section-d" className={layoutStyles.section}>
-      <div
-        className={layoutStyles.contentWrap}
-        style={{
-          backgroundImage: `url(${patch})`,
-          backgroundSize: "100% 100%",
-          backgroundRepeat: "no-repeat",
-          borderRadius: "25px",
-          paddingTop: "20px",
-        }}
-      >
-        <Grid container>
-          <Grid item xs={12} md={6}>
-            <Grid
-              container
-              justify="center"
-              className={layoutStyles.factsContainer}
-            >
-              {hardNumbers2.map(item => {
-                return (
-                  <Grid
-                    key={item.value}
-                    item xs={6}
-                    className={layoutStyles.fact}
-                  >
-                    <div className={layoutStyles.card2}>
-                      <h3>{item.value}</h3>
-                      <h4>{item.unit}</h4>
-                    </div>
-                  </Grid>
-                )
-              })}
+            <Grid item xs={12} md={6}>
+              <div className={layoutStyles.sideFacts}>
+                <h2>
+                  Viel hilft viel: <br /> Ein Projekt der großen Zahlen
+              </h2>
+                <p>
+                  Nicht nur von der schieren Masse an verschiedenen Disziplinen, sondern auch von der schieren Masse an... <b>Masse</b> ist dieses Projekt einfach riesig.
+              </p>
+              </div>
             </Grid>
           </Grid>
-
-          <Grid item xs={12} md={6}>
-            <div className={layoutStyles.sideFacts}>
-              <h2>
-                Viel hilft viel: <br /> Ein Projekt der großen Zahlen
-              </h2>
-              <p>
-                Nicht nur von der schieren Masse an verschiedenen Disziplinen, sondern auch von der schieren Masse an... <b>Masse</b> ist dieses Projekt einfach riesig.
-              </p>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
-    </section>
-
-    {/* SECTION E: CALL TO ACTION */}
-    <section id="section-e" className={layoutStyles.section}>
-      <div className={layoutStyles.contentWrap}>
-        <h2 id="section-e" style={{ padding: "80px 0px 0px" }}>
-          Und?
-        </h2>
-        <h3 className={layoutStyles.question}>Lust was zusammen zu machen?</h3>
-        <div className={layoutStyles.centered}>
-          <Link to="/contact">
-            <button type="button" className={layoutStyles.button}>
-              <span>Kontakt</span>
-            </button>
-          </Link>
         </div>
-      </div>
-    </section>
-  </Layout>
-)
+      </section>
+
+      {/* SECTION E: CALL TO ACTION */}
+      <section id="section-e" className={layoutStyles.section}>
+        <div className={layoutStyles.contentWrap}>
+          <h2 id="section-e" style={{ padding: "80px 0px 0px" }}>
+            Und?
+        </h2>
+          <h3 className={layoutStyles.question}>Lust was zusammen zu machen?</h3>
+          <div className={layoutStyles.centered}>
+            <Link to="/contact">
+              <button type="button" className={layoutStyles.button}>
+                <span>Kontakt</span>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
